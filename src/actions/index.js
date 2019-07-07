f1c3d4b24b020524b6e03c8b1045d8e0d6261d32
damienteo <ksteo89@gmail.com>
2019-07-07 23:30:53 +0800

    Show retrieved posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
 export const fetchPosts = () => async dispatch => {
-  const payload = await jsonPlaceHolder.get("./posts");
+  const { data: payload } = await jsonPlaceHolder.get("./posts");
   dispatch({ type: "FETCH_POSTS", payload });
 };
--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import PostList from "./postList";
 const App = () => {
   return (
     <div className="ui container">
-      App
       <PostList />
     </div>
   );

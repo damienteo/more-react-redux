550b72a32656378ca440f68de251649ac627b6d2
damienteo <ksteo89@gmail.com>
2019-07-07 21:51:59 +0800

    Refactor fetchPosts component to remove redundant code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,6 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
-export const fetchPosts = () => {
-  return async function(dispatch, getState) {
-    const response = await jsonPlaceHolder.get("./posts");
-    dispatch({ type: "FETCH_POSTS", payload: response });
-  };
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceHolder.get("./posts");
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
